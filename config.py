fff0f38c6a22bceaa0f7817d87b1b46e30726a00
l3chu6a <l3chu6a@gmaile.com>
2025-07-02 19:12:46 -0300

    quinto commit

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
+    SECRET_KEY = os.getenv("SECRET_KEY", "superdash_123")
